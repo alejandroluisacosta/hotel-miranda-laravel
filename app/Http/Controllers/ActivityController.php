1fa69ab36bca8c28a2e2f9928fdec35749ea486e
Alejandro <alejandroluisacosta95@gmail.com>
2024-09-05 09:01:17 +0200

    add initial commit with laravel, database connection and api rest controllers

---
+++ app/Http/Controllers/ActivityController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Activity;
+use Illuminate\Http\Request;
+
+class ActivityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $activities = Activity::all();
+        return response()->json(["activities" => $activities]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'type' => ['required', 'string', 'max:255'],
+            'dateTime' => ['required', 'date'],
+            'notes' => ['required', 'string', 'max:255'],
+        ]);
+
+        $activity = Activity::create(array_merge($validated, [
+            'user_id' => Auth::id(),
+            'paid' => $request->paid ?? false,
+            'satisfaction' => $request->satisfaction ?? 0,
+        ]));
+    
+        return response()->json(["activity" => $activity]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $activity = Activity::find($id);
+    
+        if (!$activity) {
+            return response()->json(['message' => 'Activity not found'], 404);
+        }
+    
+        return response()->json(['activity' => $activity]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'type' => ['required', 'string', 'max:255'],
+            'dateTime' => ['required', 'date'],
+            'notes' => ['required', 'string', 'max:255'],
+        ]);
+
+        $activity = Activity::create(array_merge($validated, [
+            'user_id' => Auth::id(),
+            'paid' => $request->paid ?? false,
+            'satisfaction' => $request->satisfaction ?? 0,
+        ]));
+    
+        return response()->json(["activity" => $activity]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $activity = Activity::find($id);
+    
+        if (!$activity) {
+            return response()->json(['message' => 'Activity not found'], 404);
+        }
+    
+        $activity->delete();
+    
+        return response()->json(['message' => 'Activity deleted successfully']);
+    }
+}
