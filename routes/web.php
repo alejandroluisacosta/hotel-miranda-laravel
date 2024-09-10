7bc123d6f43e80d7d1346089fec7f44213ae2530
Alejandro <alejandroluisacosta95@gmail.com>
2024-09-10 13:24:53 +0200

    add routes for home, rooms, about, offers and contact

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -16,8 +20,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('activities', ActivityController::class);
 });
 
-Route::resource('activities', ActivityController::class);
+Route::get('/home', [HomeController::class, 'index'])->name('home.index');
+Route::get('/offers', [OfferController::class, 'index'])->name('offers.index');
+Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index');
+Route::get('/rooms/{id}', [RoomController::class, 'show'])->name('rooms.show');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+Route::post('contact', [ContactController::class, 'store'])->name('contact.store');
 
 require __DIR__.'/auth.php';
