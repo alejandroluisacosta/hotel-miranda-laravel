d38518aeddb1cacdd94f183888bf4a888d7316c4
Alejandro <alejandroluisacosta95@gmail.com>
2024-09-11 16:05:09 +0200

    update room controller with bookings, amenities and images

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Room;
 
 class RoomController extends Controller
 {
     public function index() {
-        return view('rooms.rooms');
+        $rooms = Room::with(['bookings', 'amenities', 'images'])->get();
+        return view('rooms.rooms', ['rooms' => $rooms]);
     }
 
     public function show(string $id) {
