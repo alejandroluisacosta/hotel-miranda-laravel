b2ea2351e7f2ad950ca75c40a3439622c2dd5941
Alejandro <alejandroluisacosta95@gmail.com>
2024-09-11 15:49:29 +0200

    add activity and image models with their respective relationship to rooms

--- app/Models/Amenity.php
+++ app/Models/Amenity.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Room;
 
-class Booking extends Model
+class Amenity extends Model
 {
     use HasFactory;
 
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Booking;
+use App\Models\Amenity;
+use App\Models\Image;
 
 class Room extends Model
 {
@@ -14,5 +16,15 @@ class Room extends Model
     public function bookings(): HasMany
     {
         return $this->hasMany(Booking::class);
+    }
+
+    public function amenities(): HasMany
+    {
+        return $this->hasMany(Amenity::class);
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(Amenity::class);
     }
 }
